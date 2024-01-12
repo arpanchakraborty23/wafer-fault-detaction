ff85e282b0c036552006df4adc8487e2dc3f3f0e
Arpan <arpanchakraborty500@gmail.com>
2024-01-12 23:04:56 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='wafer-Fault',
+    version='0.0.1',
+    author='Arpan',
+    Author_email='arpanchakraborty500@gmail.com',
+    install_requries=[],
+    packages=find_packages()
+)
